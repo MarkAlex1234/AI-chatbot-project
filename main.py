49165f3e5334c4df4988a937b646f7f22f2472db
Mark Alexander <mark.alexander1314@gmail.com>
2023-09-18 19:25:19 +1000

    update imports

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+import os
 import openai
-import gradio
+import gradio as gr
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+api_key = os.getenv('OPENAI_API_KEY')
 
 def CustomChatGPT(user_input):
+    messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
     messages.append({"role": "user", "content": user_input})
+    
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = messages
+        api_key=api_key,
+        model="gpt-3.5-turbo",
+        messages=messages
     )
+    
     ChatGPT_reply = response["choices"][0]["message"]["content"]
-    messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
-
+demo = gr.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Real Estate Pro")
 demo.launch(share=True)
